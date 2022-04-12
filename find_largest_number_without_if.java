7a3a4b437f94ddfac3e839a2ad0a37ff11374096
Digvijay <dtikka6@gmail.com>
2022-04-12 10:01:53 +0530

    First

---
+++ find_largest_number_without_if.java
@@ -0,0 +1,17 @@
+package Programs;
+
+public class find_largest_number_without_if {
+
+
+    public static void main(String[] args) {
+
+
+        int a=20;
+        int b=30;
+
+      int lagre=  a>b?a:b; // This is using Ternary Operator
+
+        System.out.println(lagre);
+
+    }
+}
