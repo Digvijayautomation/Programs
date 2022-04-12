226e7a35e6135dfdb7a3856e394a43edc1530e28
Digvijay <dtikka6@gmail.com>
2022-04-12 14:53:12 +0530

    14-04_22

---
+++ to_check_gievn_number_is_in_array.java
@@ -0,0 +1,42 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class to_check_gievn_number_is_in_array {
+
+    public static void main(String[] args) {
+
+
+        // Practice program 2
+        System.out.println(" Find the Given Value of Day is present in array or not ");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number To Search In Array");
+        int  num_to_search =sc.nextInt();
+
+        int [] day={1,4,5,7,2};
+
+
+        boolean IsInArray=false;
+
+
+        for(float element:day) {
+            if (num_to_search == element) {
+                IsInArray = true;
+                break;
+
+            }
+        }
+        if(IsInArray)
+        {
+            System.out.println(+num_to_search+" \tIs On The Array List");
+        }
+        else
+        {
+            System.out.println(+num_to_search+" \tIs On Not In The Array List");
+        }
+
+        System.out.println("************|Ends Here|**********");
+
+    }
+}
