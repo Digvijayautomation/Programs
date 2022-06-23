80af1e4f179403ed20e175e8707238b18b6654bf
Digvijay <dtikka6@gmail.com>
2022-06-23 18:25:40 +0530

    01-06-22

--- Interview_How_to_find_string_from_other_string.java
+++ Interview_How_to_find_string_from_other_string.java
@@ -1,10 +1,33 @@
 package Programs;
 
 public class Interview_How_to_find_string_from_other_string {
+    public static void main(String[] args) {
+        
+        // To find the string
+        String str1 = "which hinge is working";
+        String   str2 = "hi";
+        int Repeat_count = 0;
+      
 
-    String str1 = "which hinge is working";
-    String   str2 = "hi";
 
+        for(int i=0;i<str1.length();i++)
+        {
+            String temp="";
+            temp+=str1.charAt(i);
+            
+            for (int j=i+1;j< str1.length();j++)
+            {
+                temp+=str1.charAt(j);
+                
+                if(temp.equalsIgnoreCase(str2))
+                {
+                    Repeat_count++;
+                }
+            }
+        }
 
+        System.out.println(Repeat_count);
+    }
+   
 
 }
--- Interview_How_to_remove_duplicate_string.java
+++ Interview_How_to_remove_duplicate_string.java
@@ -3,13 +3,10 @@ package Programs;
 import java.util.HashSet;
 import java.util.Set;
 
-public class How_to_remove_duplicate_string {
+public class Interview_How_to_remove_duplicate_string {
 
     public static void main(String[] args) {
-
-
-
-
+        
              String s1="ALTIMATIK";
 
         Set<String> hash_Set = new HashSet<String>(); // we will use Set because set will only add unique values
